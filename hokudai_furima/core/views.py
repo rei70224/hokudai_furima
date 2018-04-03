629b03a195c7ec5734fe408c384983a6e74e40c3
TetsuFe <tetsubeg1ner27@gmail.com>
2018-04-04 01:00:42 +0900

    Revert "add: ホームに最新商品を表示するよう変更"
    This reverts commit 1bc4d479388571629fc2353b357da98c9adedc7b.

--- hokudai_furima/core/views.py
+++ hokudai_furima/core/views.py
@@ -3,7 +3,7 @@ from hokudai_furima.product.models import Product
 
 # Create your views here.
 def home(request):
-    latest_products = Product.objects.all().order_by('-created_date')[:16]
+    #latest_products = Product.objects.order_by('created_date')[:10]
     #return render(request, 'home.html', {'latest_products': latest_products})
-    return render(request, 'home.html', {'product_list': latest_products})
+    return render(request, 'home.html')
 
