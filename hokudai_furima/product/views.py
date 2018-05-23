c28cd9e646fa36c168797c466e1a6cda40769021
TetsuFe <tetsubeg1ner27@gmail.com>
2018-05-23 21:48:28 +0900

    refactor: printとコメントを削除

--- hokudai_furima/product/views.py
+++ hokudai_furima/product/views.py
@@ -6,7 +6,6 @@ from django.contrib import messages
 from django.conf import settings
 from hokudai_furima.chat.models import Talk, Chat
 from hokudai_furima.chat.forms import TalkForm
-# Create your views here.
 from django.http import HttpResponse
 from functools import reduce
 import os
@@ -20,41 +19,24 @@ def product_list(request):
     products = product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'product/product_list.html', {'products': products})
 
+
 def create_product(request):
-    print("creating...")
     if request.method == "POST":
-        print("form is posted")
         form = NewProductForm(request.POST, request.FILES)
-        print(form)
         if form.is_valid():
-            print("saving...")
             product = form.save(commit=False)
             product.seller = request.user
             product.published_date = timezone.now()
             product.save()
-            print("saved")
             messages.success(request, '出品に成功しました')
             return redirect('product:product_details', pk=product.pk)
-        else:
-            """
-            if request.user.is_authenticated:
-                form = NewProductForm()
-                return render(request, 'product/create_product.html', {'form': form})
-            else:
-                # renderだとフォームが描画されない
-                #return render(request, 'main/login.html')
-                return redirect(settings.LOGIN_URL)
-            """
-            pass
-
     else:
         if request.user.is_authenticated:
             form = NewProductForm()
             return render(request, 'product/create_product.html', {'form': form})
         else:
-            # renderだとフォームが描画されない
-            #return render(request, 'main/login.html')
             return redirect(settings.LOGIN_URL)
+
 
 def update_product(request, product_pk):
     if request.method == "POST":
@@ -74,38 +56,24 @@ def update_product(request, product_pk):
             if product_seller_id == request.user.id:
                 form = UpdateProductForm(instance=product)
                 return render(request, 'product/update_product.html', {'form': form})
-            # renderだとフォームが描画されない
-            #return render(request, 'main/login.html')
         return redirect(settings.LOGIN_URL)
 
 
 def product_details(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    print(product.wanting_users)
     talk_form = TalkForm()
-    #return render(request, 'product/product_details.html', {'product': product})
     talk_records = Chat.objects.filter(product=product)
     if talk_records.exists():
-        print("talk_record: "+str(talk_records))
-        print(list(map(lambda x: x.talk_set.all(),list(talk_records))))
-        #talk_objects = list(map(lambda x: x.talk_set.all(),list(talk_records)))
-        #print(list(map(lambda x: x.sentence,list(talk_objects[0]))))
         talks = list(map(lambda x: x.talk_set.all(),list(talk_records)))[0]
     else:
         talks = []
     
     wanting_users = product.wanting_users.all()
-    print("wanting")
-    print(wanting_users)
-
-        #print("talk_record: "+str(''.join(reduce(lambda x,y: x['talk']+y['talk'],list(talk_records)))))
     if request.user.is_authenticated:
         return render(request, 'product/product_details.html', {'product': product, 'form': talk_form, 'talks':talks, 'wanting_users': wanting_users})
     else:
         return render(request, 'product/product_details.html', {'product': product, 'talks':talks})
 
-#def is_same_user(product_seller_id, login_user):
-#    return login_user.id  == product_seller_id
 
 @login_required
 def want_product(request, pk):
@@ -123,7 +91,6 @@ def want_product(request, pk):
         return HttpResponse('can\'t accept GET request')
 
 
-
 def want_product_done(request, pk):
     product = get_object_or_404(Product, pk=pk)
     wanting_products = Product.objects.filter(wanting_users=request.user)
@@ -134,7 +101,6 @@ def want_product_done(request, pk):
 def cancel_want_product(request, pk):
     product = get_object_or_404(Product, pk=pk)
     product.wanting_users.remove(request.user)
-    print(product.__dict__)
     return redirect('product:product_details', pk=product.pk)   
 
 
@@ -145,7 +111,6 @@ def product_direct_chat(request, product_pk, wanting_user_pk):
     if request.user == wanting_user or request.user == product.seller:
         talk_form = TalkForm()
         chat = Chat.objects.filter(product=product, product_wanting_user=wanting_user, product_seller=product.seller)
-        print(chat)
         if chat.exists():
             talks = list(map(lambda x: x.talk_set.all(),list(chat)))[0]
         else:
@@ -159,6 +124,7 @@ def product_direct_chat(request, product_pk, wanting_user_pk):
         return render(request, 'product/product_direct_chat.html', {'product': product, 'form': talk_form, 'talks':talks, 'wanting_user': wanting_user, 'chat': chat, 'talk_reciever_id': talk_reciever_id})
     else:
         return HttpResponse('invalid request')
+
 
 @login_required
 def decide_to_sell(request, product_pk, wanting_user_pk):
