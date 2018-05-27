9e3207f8b74107a778c92cf4f8705543bff8afae
TetsuFe <tetsubeg1ner27@gmail.com>
2018-05-28 02:46:25 +0900

    update: 商品画像のモデル構造の変更とそれに伴う変更

--- hokudai_furima/product/models.py
+++ hokudai_furima/product/models.py
@@ -6,6 +6,7 @@ from versatileimagefield.fields import PPOIField, VersatileImageField
 import os
 from versatileimagefield.placeholder import OnDiscPlaceholderImage
 
+
 class Product(models.Model):
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title  = models.CharField(max_length=200)
@@ -14,27 +15,6 @@ class Product(models.Model):
     is_sold = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(blank=True, null=True)
-    image0 = VersatileImageField(
-        '',
-        upload_to='product', 
-        blank=False,
-        null=True
-    )
-    image1 = VersatileImageField(
-        '',
-        upload_to='product',
-        blank=True,
-    )
-    image2 = VersatileImageField(
-        '',
-        upload_to='product',
-        blank=True,
-    )
-    image3 = VersatileImageField(
-        '',
-        upload_to='product',
-        blank=True,
-    )
     wanting_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='wanting_users')
 
     def update(self):
@@ -44,10 +24,19 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+class ProductImage(models.Model):
+    image = VersatileImageField(
+        '',
+        upload_to='product', 
+        blank=True,
+        null=True
+    )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def update(self):
+        self.save()
+
     @property
-    def image0_thumbnail_url(self):
-        if self.image0 and hasattr(self.image0, 'url'):
-            return self.image0.thumbnail['600x600'].url
-
-
-
+    def thumbnail_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.thumbnail['600x600'].url
