aa391860641a546bae0a9c7ce18054d8709ca31b
TetsuFe <tetsubeg1ner27@gmail.com>
2018-05-31 22:16:30 +0900

    add: 検索フォームに条件を追加

--- hokudai_furima/search/forms.py
+++ hokudai_furima/search/forms.py
@@ -1,4 +1,7 @@
 from django import forms
 
 class SearchProductForm(forms.Form):
+    PRICE_RANGE_OPTIONS = (('0_1000000', '全て'), ('0_1000','0 ~ 1000'), ('1000_3000', '1000 ~ 3000'), ('3000_5000', '3000 ~ 5000'), ('5000_10000', '5000 ~ 10000'), ('10000_1000000', '10000 ~ '))
     q = forms.CharField(label="検索ワード")
+    price_range = forms.ChoiceField(label="価格帯", choices=PRICE_RANGE_OPTIONS)
+    is_sold = forms.ChoiceField(label="販売中/売り切れ", choices=(('all', '全て'), ('sold_out', '売り切れ'), ('not_sold', '販売中')))
--- hokudai_furima/search/views.py
+++ hokudai_furima/search/views.py
@@ -4,23 +4,46 @@ from hokudai_furima.product.models import Product
 from .forms import SearchProductForm
 from django.contrib import messages
 from django.conf import settings
+from django.http import HttpResponse
 
 def search_product(request):
     if request.method == "GET":
-        form = SearchProductForm(request.GET)
-        query = request.GET.get("q")
-        if query:
-            query_words = query.split(' ')
-            search_results = []
-            if query_words is None:
-                query_words = query
-            for word in query_words:
-                results_by_word = Product.objects.filter(title__icontains=word).order_by('updated_date')
-                for result in results_by_word:
-                    if result not in search_results:
-                        search_results.append(result)
+        if request.GET.get("q"):
+            form = SearchProductForm(request.GET)
+            search_results = search(request)
             return render(request, 'search/product/search_product.html', {'product_list': search_results, 'form': form, 'is_searched': True})
 
     form = SearchProductForm()
     return render(request, 'search/product/search_product.html', {'form': form, 'is_searched': False})
 
+def search(request):
+    query_keyword_string, is_sold, price_range_pair = parse_search_request(request)
+    if not query_keyword_string:
+        return []
+    else:
+        query_words = query_keyword_string.split(' ')
+        search_results = []
+        if query_words is None:
+            query_words = query
+        for word in query_words:
+            if is_sold == 'all':
+                results_by_word = Product.objects.filter(title__icontains=word, price__gte=price_range_pair[0], price__lt=price_range_pair[1]).order_by('updated_date')
+            elif is_sold == 'sold_out':
+                results_by_word = Product.objects.filter(title__icontains=word, price__gte=price_range_pair[0], price__lt=price_range_pair[1], is_sold=True).order_by('updated_date')
+            elif is_sold == 'not_sold': 
+                results_by_word = Product.objects.filter(title__icontains=word, price__gte=price_range_pair[0], price__lt=price_range_pair[1], is_sold=False).order_by('updated_date')
+            else:
+                return HttpResponse('invalid request')
+            for result in results_by_word:
+                if result not in search_results:
+                    search_results.append(result)
+        return search_results
+
+def parse_search_request(request):
+    query_keyword_string = request.GET.get("q")
+    is_sold = request.GET.get("is_sold")
+    if request.GET.get("price_range"):
+        price_range_pair = [float(x) for x in request.GET.get("price_range").split('_')]
+    else:
+        price_range_pair = None
+    return (query_keyword_string, is_sold, price_range_pair)
