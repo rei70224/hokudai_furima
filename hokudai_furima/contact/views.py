cb632d709f614bf3f4a0f52a6fff4c0a00417389
horikaw <namakou0517@gmail.com>
2018-06-18 14:33:58 +0900

    メッセージの仮修正

--- hokudai_furima/contact/views.py
+++ hokudai_furima/contact/views.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from .forms import ContactForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -6,7 +7,7 @@ def post_contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
         form.save()
-        messages.success(request, "メッセージが送信されました")
+        messages.success(request, "お問い合わせ内容を送信しました。お問い合わせに対する対応は、OOでお知らせいたします。")
         return redirect('contact:post_success') # redirect('アプリケーション名:メソッド名')
     return render(request, 'contact/post_contact.html', {'form': form})
 
