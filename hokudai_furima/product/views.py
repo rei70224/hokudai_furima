423bb78e84a82a17959d3e5771814077998faff0
TetsuFe <tetsubeg1ner27@gmail.com>
2018-06-24 03:02:32 +0900

    update: 商品の受け取り完了ボタン押下時にユーザ評価ページに飛ぶよう変更

--- hokudai_furima/product/views.py
+++ hokudai_furima/product/views.py
@@ -242,7 +242,7 @@ def complete_to_recieve(request, product_pk):
         buyer_rating_todo.save()
 
         messages.success(request, '商品の受け取り処理がを完了しました。最後に出品者を評価してください。')
-        return redirect('product:product_details', pk=product.pk)   
+        return redirect('rating:post_rating', product_pk=product.pk)
     else:
         return HttpResponse('invalid request')
 
