a6198ad3de876e697d196e8e5fd66e7cd6b4c3a4
horikaw <namakou0517@gmail.com>
2018-06-29 12:48:52 +0900

    add: emailフィールドの追加

--- hokudai_furima/contact/forms.py
+++ hokudai_furima/contact/forms.py
@@ -4,4 +4,4 @@ from .models import Contact
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ('text',)
+        fields = ('email', 'text')
--- hokudai_furima/contact/models.py
+++ hokudai_furima/contact/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Contact(models.Model):
     text = models.TextField(('contact'), max_length=1000)
+    email = models.EmailField(('email'), default='')
