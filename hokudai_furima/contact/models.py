cf1835e17f29483f83aaab63f17b651f46232784
horikaw <namakou0517@gmail.com>
2018-07-01 00:05:53 +0900

    fix: 文面の修正

--- hokudai_furima/contact/models.py
+++ hokudai_furima/contact/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Contact(models.Model):
-    text = models.TextField(('contact'), max_length=1000)
+    text = models.TextField(('お問い合わせ内容'), max_length=1000)
     email = models.EmailField(('メールアドレス'), default='')
