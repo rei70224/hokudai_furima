e5a35d65f86e4fffc1bb731553a511a3d91d4a94
horikaw <namakou0517@gmail.com>
2018-06-30 23:42:03 +0900

    fix: 文面の修正

--- hokudai_furima/contact/models.py
+++ hokudai_furima/contact/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Contact(models.Model):
     text = models.TextField(('contact'), max_length=1000)
-    email = models.EmailField(('email'), default='')
+    email = models.EmailField(('メールアドレス'), default='')
