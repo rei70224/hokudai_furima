d30ec183f61a4f02f94f84eacd230103fe56cce2
TetsuFe <tetsubeg1ner27@gmail.com>
2018-08-10 14:36:31 +0900

    update: パラメータをいくつか持ったページでのページネーションに対応

--- hokudai_furima/product/templatetags/show_product_list.py
+++ hokudai_furima/product/templatetags/show_product_list.py
@@ -1,6 +1,8 @@
 from django import template
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.conf import settings
+from django.urls import reverse
+import re
 
 register = template.Library()
 
@@ -9,4 +11,11 @@ def show_product_list(request, product_list):
     paginator = Paginator(product_list, settings.PRODUCT_NUM_PER_PAGE)
     page = request.GET.get('page')
     paginated_product_list = paginator.get_page(page)
-    return {'product_list': paginated_product_list}
+    url_with_params = re.sub('(\?|&)page=\d+','',request.get_full_path())
+    print(url_with_params)
+    if '?' in url_with_params:
+        # パラメータがあれば、次につけるパラメータは&page=1などになる。パラメータがなければ?page=1などになる。?か&を付与する
+        url_with_params += '&'
+    else:
+        url_with_params += '?'
+    return {'product_list': paginated_product_list, 'url_with_params': url_with_params}
