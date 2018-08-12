4bc9cda2347f19fa27d4bbe56899b656e46ca87e
TetsuFe <tetsubeg1ner27@gmail.com>
2018-08-12 22:41:44 +0900

    update: ユーザ評価ラジオボタンの表示順の変更

--- hokudai_furima/rating/forms.py
+++ hokudai_furima/rating/forms.py
@@ -3,8 +3,8 @@ from .models import UserRating
 
 class UserRatingForm(forms.Form):
     RATING_CHOICES = (
-        ('bad', '悪い'),
-        ('normal', '普通'),
         ('good', '良い'),
+        ('normal', '普通'),
+        ('bad', '悪い'),
     )
     rating = forms.ChoiceField(label='評価', widget=forms.RadioSelect, choices=RATING_CHOICES)
