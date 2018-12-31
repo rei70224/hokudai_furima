7d6959db8efe37ad3289f98aa6010af8cce96e1a
TetsuFe <tetsubeg1ner27@gmail.com>
2018-12-31 16:15:44 +0900

    Add watchlist model

--- hokudai_furima/watchlist/models.py
+++ hokudai_furima/watchlist/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from hokudai_furima.product.models import Product
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class WatchList(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='watching_user')
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(blank=True, null=True)
+
+    def update(self):
+        self.updated_date = timezone.now()
+        self.save()
