78d32e4ead0ac30e5ce4fab84f79e9aab1b02025
TetsuFe <tetsubeg1ner27@gmail.com>
2019-01-02 23:50:20 +0900

    Add add watchlist ajax functions and api

--- hokudai_furima/watchlist/urls.py
+++ hokudai_furima/watchlist/urls.py
@@ -5,4 +5,5 @@ app_name = "watchlist"
 
 urlpatterns = [
     path('', views.show_watch_list, name='show_watch_list'),
+    path('add', views.add_watch_list, name='add_watch_list'),
 ]
--- hokudai_furima/watchlist/views.py
+++ hokudai_furima/watchlist/views.py
@@ -13,3 +13,17 @@ def show_watch_list(request):
         return render(request, 'watchlist/watchlist.html', {'watchlist_product_list': watch_list.product_set.all()})
     except WatchList.DoesNotExist:
         return render(request, 'watchlist/no_watchlist.html')
+
+
+@login_required
+def add_watch_list(request):
+    try:
+        watch_list = WatchList.objects.get(user=request.user)
+    except WatchList.DoesNotExist:
+        watch_list = WatchList(user=request.user)
+        watch_list.save()
+    product_pk = request.POST.get('product_pk')
+    product = Product.objects.get(pk=product_pk)
+    watch_list.product_set.add(product)
+    watch_list.update()
+    return JsonResponse({'success': True})
