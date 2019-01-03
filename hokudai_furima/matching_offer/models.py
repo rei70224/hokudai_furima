d4563d37968b993c9d505a7ebd34d7a1ec641c18
TetsuFe <tetsubeg1ner27@gmail.com>
2019-01-04 01:08:25 +0900

    Add matching offer and talk models

--- hokudai_furima/matching_offer/models.py
+++ hokudai_furima/matching_offer/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from hokudai_furima.watchlist.models import WatchList
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
+class MatchingOffer(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=512)
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, default=None)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.SET_NULL, null=True, default=None)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(blank=True, null=True)
+
+
+class MatchingOfferTalk(models.Model):
+    talker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    text = models.TextField(max_length=512)
+    matchingOffer = models.ForeignKey(MatchingOffer, on_delete=models.CASCADE, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
