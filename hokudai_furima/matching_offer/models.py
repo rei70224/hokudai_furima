103f11d50c98e59de2eb62a48a4335e894115a67
TetsuFe <tetsubeg1ner27@gmail.com>
2019-01-04 23:14:37 +0900

    Add image models of matching offer

--- hokudai_furima/matching_offer/models.py
+++ hokudai_furima/matching_offer/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from hokudai_furima.watchlist.models import WatchList
 from django.utils import timezone
 from django.conf import settings
+from versatileimagefield.fields import VersatileImageField
 
 # Create your models here.
 class MatchingOffer(models.Model):
@@ -11,6 +12,29 @@ class MatchingOffer(models.Model):
     watchlist = models.ForeignKey(WatchList, on_delete=models.SET_NULL, blank=True, null=True, default=None)
     created_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(blank=True, null=True)
+
+
+class MatchingOfferImage(models.Model):
+    image = VersatileImageField(
+        '',
+        upload_to='matching_offer',
+        blank=True,
+        null=True
+    )
+    matching_offer = models.ForeignKey(MatchingOffer, on_delete=models.CASCADE)
+
+    def update(self):
+        self.save()
+
+    @property
+    def thumbnail_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.thumbnail['600x600'].url
+
+    @property
+    def thumbnail_100_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.thumbnail['100x100'].url
 
 
 class MatchingOfferTalk(models.Model):
