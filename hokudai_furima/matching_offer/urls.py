20c806f6f3f9f2b64c53ab2ac9af1c13c74b766a
TetsuFe <tetsubeg1ner27@gmail.com>
2019-01-04 20:27:07 +0900

    Add create offer talk functions and templates

--- hokudai_furima/matching_offer/urls.py
+++ hokudai_furima/matching_offer/urls.py
@@ -5,6 +5,7 @@ app_name = "matching_offer"
 
 urlpatterns = [
     re_path(r'^(?P<pk>\d+)/$', views.matching_offer_details, name='matching_offer_details'),
+    path('chat/create', views.create_offer_talk, name='create_offer_talk'),
     #path('create', views.create_matching_offer, name='create_matching_offer'),
     #re_path(r'^update/(?P<matching_offer_pk>\d+)/$', views.update_matching_offer, name='update_matching_offer'),
 ]
