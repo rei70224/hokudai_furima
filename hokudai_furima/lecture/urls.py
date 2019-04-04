eeea490c9eac8ac8c945ef51279e993c0a0d2584
TetsuFe <tetsubeg1ner27@gmail.com>
2019-04-05 02:56:05 +0900

    Update lecture category list system

--- hokudai_furima/lecture/urls.py
+++ hokudai_furima/lecture/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "lecture"
 
 urlpatterns = [
-    path('details/(<int:pk>)/', views.lecture_category_details, name='lecture_category_details'),
+    path('details/<int:pk>/', views.lecture_category_details, name='lecture_category_details'),
     path('list/', views.lecture_category_list, name='lecture_category_list'),
+    path('list/<int:pk>/', views.lecture_category_list_with_pk, name='lecture_category_list_with_pk'),
 ]
--- hokudai_furima/lecture/views.py
+++ hokudai_furima/lecture/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import LectureCategory
+from django.urls import reverse
 
 
 def lecture_category_list(request):
-    _lecture_category_list = LectureCategory.objects.all().order_by('created_date')
+    _lecture_category_list = LectureCategory.objects.filter(level=0).order_by('id')
+    return render(request, 'lecture/lecture_category_list.html', {'lecture_category_list': _lecture_category_list})
+
+
+def lecture_category_list_with_pk(request, pk):
+    lecture_category = get_object_or_404(LectureCategory, pk=pk)
+    _lecture_category_list = lecture_category.children.all().order_by('id')
     return render(request, 'lecture/lecture_category_list.html', {'lecture_category_list': _lecture_category_list})
 
 
@@ -15,9 +22,9 @@ def lecture_category_details(request, pk):
         lecture_category_parent_chain.append(temp_parent_lecture_category)
         temp_parent_lecture_category = temp_parent_lecture_category.parent
     lecture_category_parent_chain.reverse()
-    lecture_category_products = lecture_category.lecture_category_products.all().order_by('created_date')
+    lecture_category_products = lecture_category.lecture_category_products.all().order_by('id')
 
-    child_lecture_categories = lecture_category.children.all().order_by('created_date')
+    child_lecture_categories = lecture_category.children.all().order_by('id')
     return render(request, 'lecture/lecture_category_details.html',
                   {'lecture_category': lecture_category, 'lecture_category_parent_chain': lecture_category_parent_chain,
                    'child_lecture_categories': child_lecture_categories,
